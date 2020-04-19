164045711639c1abc815bba0a73ffa1383407248
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-19 20:26:32 +0600

    add image files

--- app/src/main/java/com/luminous/happybirthday/ui/home/HomeFragment.java
+++ app/src/main/java/com/luminous/happybirthday/ui/home/HomeFragment.java
@@ -5,11 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.luminous.happybirthday.R;
@@ -17,19 +19,17 @@ import com.luminous.happybirthday.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private VideoView gifBirthdayView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+        //Without ViewModelFactory
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        gifBirthdayView = () root.findViewById(R.id.gifHappyBirthDay);
+
+
         return root;
     }
 }
