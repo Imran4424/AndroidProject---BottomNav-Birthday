93891f8ff1fd7689aed333d78876381eeee33628
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-20 00:11:59 +0600

    recyler View Adapter

--- app/src/main/java/com/luminous/happybirthday/ImageRecyclerAdapter.java
+++ app/src/main/java/com/luminous/happybirthday/ImageRecyclerAdapter.java
@@ -3,17 +3,34 @@ package com.luminous.happybirthday;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ImageRecyclerAdapter {
+public class ImageRecyclerAdapter extends RecyclerView.Adapter<ImageRecyclerAdapter.ViewHolder> {
     private final Context context;
     private final LayoutInflater layoutInflater;
 
     public ImageRecyclerAdapter(Context context, LayoutInflater layoutInflater) {
         this.context = context;
         this.layoutInflater = layoutInflater;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
