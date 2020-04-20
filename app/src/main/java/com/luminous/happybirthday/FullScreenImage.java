4105a80f5dc861fd432fe9a88bd85dd16aa504e5
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-20 13:31:59 +0600

    set full screen image view layout

--- app/src/main/java/com/luminous/happybirthday/FullScreenImage.java
+++ app/src/main/java/com/luminous/happybirthday/FullScreenImage.java
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class FullScreenImage extends AppCompatActivity {
+    private Integer[] imageResArray = new Integer[] {R.drawable.shai_1,
+            R.drawable.shai_2,
+            R.drawable.shai_3,
+            R.drawable.shai_4,
+            R.drawable.shai_5,
+            R.drawable.shai_6};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
