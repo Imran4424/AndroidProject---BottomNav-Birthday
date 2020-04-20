5b5841a4a3121d07262731ca85a8b5cc98f96679
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-20 14:05:18 +0600

    set full screen image activity image resource

--- app/src/main/java/com/luminous/happybirthday/FullScreenImage.java
+++ app/src/main/java/com/luminous/happybirthday/FullScreenImage.java
@@ -2,11 +2,13 @@ package com.luminous.happybirthday;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 
 public class FullScreenImage extends AppCompatActivity {
     public static final String IMAGE_POSITION = "com.luminous.happybirthday.IMAGE_POSITION";
+    public static final int POSITION_NOT_SET = -1;
     private Integer[] imageResArray = new Integer[] {R.drawable.shai_1,
             R.drawable.shai_2,
             R.drawable.shai_3,
@@ -22,5 +24,8 @@ public class FullScreenImage extends AppCompatActivity {
         setContentView(R.layout.activity_full_screen_image);
 
         zoomImageView = (ImageView) findViewById(R.id.zoomImageView);
+
+        Intent fullScreenIntent = getIntent();
+        zoomImageView.setImageResource(imageResArray[fullScreenIntent.getIntExtra(IMAGE_POSITION, POSITION_NOT_SET)]);
     }
 }
