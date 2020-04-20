60004fe1ddbbed1224d9bec6fb923b06016b388e
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-04-20 10:33:33 +0600

    recyler View Adapter

--- app/src/main/java/com/luminous/happybirthday/ImageRecyclerAdapter.java
+++ app/src/main/java/com/luminous/happybirthday/ImageRecyclerAdapter.java
@@ -5,8 +5,10 @@ import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
@@ -14,9 +16,9 @@ import java.util.List;
 public class ImageRecyclerAdapter extends RecyclerView.Adapter<ImageRecyclerAdapter.ViewHolder> {
     private final Context context;
     private final LayoutInflater layoutInflater;
-    private final List<Image> images;
+    private final List<ImageView> images;
 
-    public ImageRecyclerAdapter(Context context, LayoutInflater layoutInflater, List<Image> images) {
+    public ImageRecyclerAdapter(Context context, LayoutInflater layoutInflater, List<ImageView> images) {
         this.context = context;
         this.layoutInflater = LayoutInflater.from(context);
         this.images = images;
@@ -31,17 +33,20 @@ public class ImageRecyclerAdapter extends RecyclerView.Adapter<ImageRecyclerAdap
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        holder.imageView.setImageDrawable(images.get(position).getDrawable());
     }
-
+    
     @Override
     public int getItemCount() {
         return images.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        public final ImageView imageView;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            imageView = (ImageView) itemView.findViewById(R.id.itemImage);
         }
     }
 }
